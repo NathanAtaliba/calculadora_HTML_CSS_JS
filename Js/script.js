4f4734713b586e71546e51d98a530a7926830d11
NathanAtaliba <100451579+NathanAtaliba@users.noreply.github.com>
2022-07-19 12:19:46 -0300

    Calculadora

---
+++ Js/script.js
@@ -0,0 +1,42 @@
+function inserir(num){
+    var numero = document.getElementById('res').innerHTML;
+    document.getElementById('res').innerHTML = numero + num;
+}
+var sub =window.document.getElementById('menos');
+
+function subtracao(){
+
+}
+function soma(){
+
+}
+function divisao(){
+
+}
+function multiplicacao(){
+
+}
+function igual(){
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var som =window.document.getElementById('mais');
+var div =window.document.getElementById('dvidir');
+var mult =window.document.getElementById('mul');
+var igual =window.document.getElementById('igual');
+
